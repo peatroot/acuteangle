e712426b4ecc2e8bc5b4e18a2c176c4a6701f981
Gareth Peat <garethpeat@gmail.com>
2020-07-07 20:57:20 +0100

    Use functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { CssBaseline } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/styles';
@@ -7,19 +7,15 @@ import theme from './theme';
 import Home from './pages/Home';
 import Screen from './components/Screen';
 
-class App extends Component {
-  render() {
-    return (
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Screen>
-          <Router>
-            <Route path="/" exact component={Home} />
-          </Router>
-        </Screen>
-      </ThemeProvider>
-    );
-  }
-}
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <Screen>
+      <Router>
+        <Route path="/" exact component={Home} />
+      </Router>
+    </Screen>
+  </ThemeProvider>
+);
 
 export default App;
